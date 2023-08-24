67edc142ffb77920c4ecc2b5f5fc3ec9e410c61e
Кирилл Кувшинов <k.koovshinov@gmail.com>
2023-08-24 17:49:53 +0300

    Turn it into a subTest thingy.

--- notes/tests/test_routes.py
+++ notes/tests/test_routes.py
@@ -14,38 +14,37 @@ class TestRoutes(TestCase):
 
     @classmethod
     def setUpTestData(cls):
-        cls.client = Client()
-        # cls.author = User.objects.create(username='Лев Толстой')
+        # cls.client = Client()
+        cls.author = User.objects.create(username='Лев Толстой')
         # cls.reader = User.objects.create(username='Читатель Простой')
         # cls.client.force_login(cls.author)
 
-        # cls.note = Note.objects.create(
-        #     title='Заголовок заметки',
-        #     text='Текст заметки',
-        #     author=cls.author,
-        # )
+        cls.note = Note.objects.create(
+            title='Заголовок заметки Льва Николаича',
+            text='Текст заметки Толстого Л.В.',
+            author=cls.author,
+        )
 
-    def test_home_page(self):
-        url = reverse('notes:home')
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, HTTPStatus.OK)
-
-    # def test_home_page_loggedin(self):
+    # def test_home_page(self):
     #     url = reverse('notes:home')
-    #     response = self.test_client.get(url)
+    #     response = self.client.get(url)
     #     self.assertEqual(response.status_code, HTTPStatus.OK)
 
-    # def test_detail_page_anonymous(self):
+    # def test_detail_page(self):
     #     url = reverse('notes:detail', args=(self.note.slug,))
     #     response = self.client.get(url)
     #     self.assertEqual(response.status_code, HTTPStatus.FOUND)
 
-    # def test_detail_page_loggedin_author(self):
-    #     url = reverse('notes:detail', args=(self.note.slug,))
-    #     response = self.test_client.get(url)
-    #     self.assertEqual(response.status_code, HTTPStatus.OK)
-
-    # def test_detail_page_loggedin_noauthor(self):
-    #     url = reverse('notes:detail', args=(self.note.slug,))
-    #     response = self.test_client2.get(url)
-    #     self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
+    def test_non_note_page_avaiability(self):
+        urls = (
+            ('notes:home', None),
+            # ('notes:detail', (self.note.slug,), HTTPStatus.FOUND),
+            ('users:login', None),
+            ('users:logout', None),
+            ('users:signup', None),
+        )
+        for name, args in urls:
+            with self.subTest(name=name):
+                url = reverse(name, args=args)
+                response = self.client.get(url)
+                self.assertEqual(response.status_code, HTTPStatus.OK)
