7d9b6a130141a809622e4159a76af1ffbbe8bf6d
Кирилл Кувшинов <k.koovshinov@gmail.com>
2023-08-27 17:36:51 +0300

    YaNotes' logic seems to 've been tested alrite.

--- notes/tests/test_logic.py
+++ notes/tests/test_logic.py
@@ -17,7 +17,7 @@ User = get_user_model()
 class TestNoteCreation(TestCase):
     NOTE_TEXT = 'Текст новой заметки'
     NOTE_TITLE = 'NewNote'
-    NOTE_TEXT_UPD = 'Текст новой заметки UPDATED'
+    NOTE_TEXT_UPD = 'Текст заметки UPDATED'
 
     @classmethod
     def setUpTestData(cls):
@@ -68,7 +68,6 @@ class TestNoteCreation(TestCase):
     def test_author_can_delete_note(self):
         self.auth_client.post(self.url, data=self.form_data)
         note = Note.objects.all()[1]  # Picking up the second note
-        # От имени автора комментария отправляем DELETE-запрос на удаление.
         response = self.auth_client.delete(
             reverse('notes:delete', args=((note.slug,)))
         )
@@ -76,10 +75,29 @@ class TestNoteCreation(TestCase):
         note_count = Note.objects.count()
         self.assertEqual(note_count, 1)  # There's only one left
 
-    def test_anonymous_user_cant_delete_comment_of_another_user(self):
+    def test_user_cant_delete_note_of_another_user(self):
         self.reader_client.force_login(self.reader)
         response = self.reader_client.delete(
             reverse('notes:delete', args=((self.note.slug,))))
         self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
         note_count = Note.objects.count()
         self.assertEqual(note_count, 1)
+
+    def test_author_can_edit_note(self):
+        self.form_data['text'] = self.NOTE_TEXT_UPD
+        response = self.auth_client.post(
+            reverse('notes:edit', args=((self.note.slug,))),
+            data=self.form_data)
+        self.assertRedirects(response, reverse('notes:success'))
+        self.note.refresh_from_db()
+        self.assertEqual(self.note.text, self.NOTE_TEXT_UPD)
+
+    def test_user_cant_edit_comment_of_another_user(self):
+        self.form_data['text'] = self.NOTE_TEXT_UPD
+        response = self.reader_client.post(
+            reverse('notes:edit', args=((self.note.slug,))),
+            data=self.form_data
+        )
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+        self.note.refresh_from_db()
+        self.assertEqual(self.note.text, TestNoteCreation.note.text)
