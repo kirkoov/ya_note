21931e242338c1944ba72f3515b60da8b8691d64
Кирилл Кувшинов <k.koovshinov@gmail.com>
2023-08-27 13:57:01 +0300

    Getting ready b4 testing the logic.

---
+++ notes/tests/test_logic.py
@@ -0,0 +1,11 @@
+# from http import HTTPStatus
+
+# from django.contrib.auth import get_user_model
+# from django.test import Client, TestCase
+# from django.urls import reverse
+
+# from news.forms import BAD_WORDS, WARNING
+# from news.models import Comment, News
+
+# User = get_user_model()
+
